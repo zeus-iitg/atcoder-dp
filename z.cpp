2be1b2699362b7fdd3ce2017ba83c0a66ea7160d
Vaibhav Kumar Singh <vaibhavsingh878@gmail.com>
2021-01-19 22:21:54 +0530

    Create z.cpp

---
+++ z.cpp
@@ -0,0 +1,131 @@
+#include <bits/stdc++.h>
+#include <ext/pb_ds/assoc_container.hpp>
+#include <ext/pb_ds/tree_policy.hpp>
+#define pb push_back
+#define IOS ios_base::sync_with_stdio(0);cin.tie(0);cout.tie(0);
+#define ll long long
+#define gcd(a,b) __gcd((a),(b))
+#define lcm(a,b) ((a)*(b))/__gcd((a),(b))
+#define vi vector<int>
+#define vll vector<long long>
+#define vvi vector<vi>
+#define mt make_tuple
+#define mp make_pair
+#define hell 1000000007
+#define mod 998244353
+#define pii pair<int,int>
+#define pll pair<ll,ll>
+#define vpii vector<pii>
+#define vpll vector<pll>
+#define all(a) a.begin(),a.end()
+#define ign cin.ignore();
+#define g(a,b) get<a>(b)
+#define vt(a) vector<a>
+#define rep(i,a,b) for(ll i=a;i<b;i++)
+#define repi(i,a,b) for(ll i=b-1;i>=a;i--)
+#define fi first
+#define se second
+ 
+#define o_set tree<int, null_type,less<int>, rb_tree_tag,tree_order_statistics_node_update>
+#define o_setll tree<ll, null_type,less<ll>, rb_tree_tag,tree_order_statistics_node_update>
+//to delete an element
+//s.erase(s.find_by_order(s.order_of_key(ele)));
+//where s is the name of the ordered_set
+//member functions :
+//1. order_of_key(k) : number of elements strictly lesser than k
+//2. find_by_order(k) : k-th element in the set
+ 
+using namespace std;
+using namespace __gnu_pbds;
+ 
+void __print(int x) {cerr << x;}
+void __print(long x) {cerr << x;}
+void __print(long long x) {cerr << x;}
+void __print(unsigned x) {cerr << x;}
+void __print(unsigned long x) {cerr << x;}
+void __print(unsigned long long x) {cerr << x;}
+void __print(float x) {cerr << x;}
+void __print(double x) {cerr << x;}
+void __print(long double x) {cerr << x;}
+void __print(char x) {cerr << '\'' << x << '\'';}
+void __print(const char *x) {cerr << '\"' << x << '\"';}
+void __print(const string &x) {cerr << '\"' << x << '\"';}
+void __print(bool x) {cerr << (x ? "true" : "false");}
+ 
+template<typename T, typename V>
+void __print(const pair<T, V> &x) {cerr << '{'; __print(x.first); cerr << ','; __print(x.second); cerr << '}';}
+template<typename T>
+void __print(const T &x) {int f = 0; cerr << '{'; for (auto &i: x) cerr << (f++ ? "," : ""), __print(i); cerr << "}";}
+void _print() {cerr << "]\n";}
+template <typename T, typename... V>
+void _print(T t, V... v) {__print(t); if (sizeof...(v)) cerr << ", "; _print(v...);}
+#ifndef ONLINE_JUDGE
+#define debug(x...) cerr << "[" << #x << "] = ["; _print(x)
+#else
+#define debug(x...)
+#endif
+ 
+#pragma once
+ 
+struct Line {
+	mutable ll k, m, p;
+	bool operator<(const Line& o) const { return k < o.k; }
+	bool operator<(ll x) const { return p < x; }
+};
+ 
+struct LineContainer : multiset<Line, less<>> {
+	// (for doubles, use inf = 1/.0, div(a,b) = a/b)
+	static const ll inf = LLONG_MAX;
+	ll div(ll a, ll b) { // floored division
+		return a / b - ((a ^ b) < 0 && a % b); }
+	bool isect(iterator x, iterator y) {
+		if (y == end()) return x->p = inf, 0;
+		if (x->k == y->k) x->p = x->m > y->m ? inf : -inf;
+		else x->p = div(y->m - x->m, x->k - y->k);
+		return x->p >= y->p;
+	}
+	void add(ll k, ll m) {
+		auto z = insert({k, m, 0}), y = z++, x = y;
+		while (isect(y, z)) z = erase(z);
+		if (x != begin() && isect(--x, y)) isect(x, y = erase(y));
+		while ((y = x) != begin() && (--x)->p >= y->p)
+			isect(x, erase(y));
+	}
+	ll query(ll x) {
+		assert(!empty());
+		auto l = *lower_bound(x);
+		return l.k * x + l.m;
+	}
+};
+ 
+int main()
+{
+    IOS
+    /*
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
+    */
+    ll ntc=1;
+    //cin>>ntc;
+    rep(tc,1,ntc+1)
+    {
+        //cout<<"Case #"<<tc<<": ";
+        LineContainer hull;
+        ll n, c;
+        cin>>n>>c;
+        ll h[n];
+        for(int i=0;i<n;i++){
+            cin>>h[i];
+        }
+        ll dp[n];
+        dp[0]=0;
+        hull.add(2*h[0], -h[0]*h[0] - dp[0]);
+        for(int i=1;i<n;i++){
+            dp[i]=-hull.query(h[i]);
+            dp[i]=dp[i]+h[i]*h[i]+c;
+            hull.add(2*h[i], -h[i]*h[i] - dp[i]);
+        }
+        cout<<dp[n-1]<<"\n";
+    }
+    return 0;
+}
